cf6a5b815e819e51663d4f494b5552c7446b3845
Aineah-Simiyu <76047184+Aineah-Simiyu@users.noreply.github.com>
2020-12-21 12:26:18 +0300

    Create Instagram.py

---
+++ Instagram.py
@@ -0,0 +1,32 @@
+import aiohttp
+import asyncio
+import argparse
+
+from bs4 import BeautifulSoup
+from re import findall
+
+url = 'https://www.instagram.com/'
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/87.0.4280.88 Safari/537.36 Edg/87.0.664.66 '
+}
+parser = argparse.ArgumentParser()
+parser.add_argument(dest='username', type=str, help='Enter user name to see details!')
+args = parser.parse_args()
+
+
+async def get_stats():
+    async with aiohttp.ClientSession(headers=headers) as session:
+        async with session.get(url + str(args.username)) as resp:
+            r = await resp.text()
+            soup = BeautifulSoup(r, 'html.parser')
+            meta1 = soup.find('meta', property="og:description").attrs['content']
+            data1 = meta1.split(' - ')[0]
+            Followers = data1.split(',')[0]
+            Following = data1.split(',')[1]
+            Post = findall('Following,.(.*)', str(data1))[0]
+
+            print('[+] ' + f'{Followers}\n' + '[+]' + f'{Following}\n' + '[+] ' + f'{Post}\n')
+
+
+asyncio.run(get_stats())
